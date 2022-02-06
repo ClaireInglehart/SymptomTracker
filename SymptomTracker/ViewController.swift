6d7eb09bbbc1373eac563f446c594d5193d2a2fe
justEClaire <70547689+justEClaire@users.noreply.github.com>
2022-02-06 09:49:55 -0800

    Initial Commit

---
+++ SymptomTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SymptomTracker
+//
+//  Created by Claire Inglehart on 2/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
