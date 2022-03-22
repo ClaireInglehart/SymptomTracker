87d39c395aa8809e625a9054517168c8f3b6acf5
Mike <mike.inglehart@gmail.com>
2022-03-21 20:49:35 -0700

    Add code for TDLikertScaleSelectorView

--- SymptomTracker/AppDelegate.swift
+++ SymptomTracker/AppDelegate.swift
@@ -12,7 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // Fixes issue in SVProgressHUD
     lazy var window: UIWindow? = {
-        return UIApplication.shared.windows.first
+        return UIApplication.shared.keyWindow
     }()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -40,6 +40,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
+    
+}
+
+
+extension UIApplication {
+    
+    var keyWindow: UIWindow? {
+        // Get connected scenes
+        return UIApplication.shared.connectedScenes
+            // Keep only active scenes, onscreen and visible to the user
+            .filter { $0.activationState == .foregroundActive }
+            // Keep only the first `UIWindowScene`
+            .first(where: { $0 is UIWindowScene })
+            // Get its associated windows
+            .flatMap({ $0 as? UIWindowScene })?.windows
+            // Finally, keep only the key window
+            .first(where: \.isKeyWindow)
+    }
     
 }
 
